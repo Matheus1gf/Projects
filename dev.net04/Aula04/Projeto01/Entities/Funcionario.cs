54cbffa10c4b9498998b75974a02a9eed47c74b6
Matheus1gf <matheusrj1601@gmail.com>
2019-01-17 21:42:19 -0200

    commit 1

---
+++ dev.net04/Aula04/Projeto01/Entities/Funcionario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto01.Entities
+{
+    public class Funcionario
+    {
+        public int IdFuncionario { get; set; }
+        public string Nome { get; set; }
+        public decimal Salario { get; set; }
+        public DateTime DataAdmissao { get; set; }
+         
+        //Relacionamento de Associação (TER-1)
+        public Setor Setor { get; set; }
+
+        public Funcionario()
+        {
+
+        }
+
+        public Funcionario(int idFuncionario, string nome, decimal salario, DateTime dataAdmissao)
+        {
+            IdFuncionario = idFuncionario;
+            Nome = nome;
+            Salario = salario;
+            DataAdmissao = dataAdmissao;
+        }
+    }
+}
---
+++ dev.net04/Aula04/Projeto01/Entities/Setor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto01.Entities
+{
+    public class Setor
+    {
+        //prop + 2x[tab]
+        public int IdSetor { get; set; } 
+        public string Nome { get; set; }
+
+        //Relacionamento de Associação (TER-MUITOS)
+        public List<Funcionario> Funcionarios { get; set; }
+
+        public Setor()
+        {
+                
+        }
+
+        public Setor(int idSetor, string nome)
+        {
+            IdSetor = idSetor;
+            Nome = nome;
+        }
+    }
+}
